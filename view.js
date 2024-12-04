377530cd25e9e61d5f6d54ebf8db05aff26c33c7
Gasan <gasan07.03.2009@icloud.com>
2024-12-04 12:48:00 +0300

    Delete view.js

--- view.js
+++
@@ -1,7 +0,0 @@
-Эти ссылки пригодятся для работы:
-
-1) https://jsonplaceholder.typicode.com/
-
-2) https://www.omdbapi.com/
-
-3) https://chrome.google.com/webstore/detail/json-formatter/bcjindcccaagfpapjjmafapmmgkkhgoa
